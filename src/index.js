b98128e406a1d1fae84a2637985ed3f96bb7e367
Vladislav <superiorite.vladislav@gmail.com>
2020-03-16 12:13:32 +0500

    Simple timer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,68 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Timer = (props) => {
+  if (props.timeLeft === 0) {
+    document.getElementById('end-of-time').play()
+  }
+  if (props.timeLeft === null || props.timeLeft === 0)
+    return <div/>
+  return <h1>Time left: {props.timeLeft}</h1>
+}
+
+class Button extends React.Component {
+  startTimer(event) {
+    return this.props.startTimer(this.props.time)
+  }
+  render() {
+    return <button
+      type="button"
+      className='btn-default btn'
+      onClick={()=>{this.props.startTimer(this.props.time)}}>
+      {this.props.time} seconds
+    </button>
+  }
+}
+
+class TimerWrapper extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state =  {timeLeft: null, timer: null}
+    this.startTimer = this.startTimer.bind(this)
+  }
+  startTimer(timeLeft) {
+    clearInterval(this.state.timer)
+    let timer = setInterval(() => {
+      console.log('2: Inside of setInterval')
+      var timeLeft = this.state.timeLeft - 1
+      if (timeLeft === 0) clearInterval(timer)
+      this.setState({timeLeft: timeLeft})
+    }, 1000)
+    console.log('1: After setInterval')
+    return this.setState({timeLeft: timeLeft, timer: timer})
+  }
+  render() {
+    return (
+      <div className="row-fluid">
+        <h2>Timer</h2>
+        <div className="btn-group" role="group" >
+          <Button time="5" startTimer={this.startTimer}/>
+          <Button time="10" startTimer={this.startTimer}/>
+          <Button time="15" startTimer={this.startTimer}/>
+        </div>
+        <Timer timeLeft={this.state.timeLeft}/>
+      <audio id="end-of-time" src="flute_c_long_01.wav" preload="auto"></audio>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+	<TimerWrapper />, 
+	document.getElementById('root')
+	);
+
+
